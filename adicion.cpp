a9cc085c65e5cd9fa37f72778e4f3fb3f859da80
Mariano Cortina <mncort@users.noreply.github.com>
2017-04-06 10:07:36 -0300

    Add files via upload

---
+++ adicion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+int main() {
+
+    int a,b;
+
+    std::cout<<"ingrese dos numeros para realizar la adicion: ";
+    std::cin>>a>>b;
+    std::cout<<"El resultado de la suma de ambos numeros es: "<<a+b;
+}
